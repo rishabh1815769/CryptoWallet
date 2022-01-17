a1a8b2f42b3f66ba6669689472d48cf2a42fe09c
Rishabh <rishabh1815769@gmail.com>
2022-01-17 17:28:15 +0530

    Initial Commit

---
+++ constants/dummy.js
@@ -0,0 +1,35 @@
+export const holdings = [
+  {
+    id: "shiba-inu",
+    qty: 1000,
+  },
+  {
+    id: "ethereum",
+    qty: 0.1,
+  },
+  {
+    id: "dogecoin",
+    qty: 100,
+  },
+];
+
+export const profile = {
+  id: 84857689347,
+  email: "rishabh1815769@gmail.com",
+};
+
+export const settings = {
+  launchScreen: "Home",
+  currency: "INR",
+  appearance: "Dark",
+  language: "English",
+  faceId: true,
+};
+
+const dummyData = {
+  holdings,
+  profile,
+  settings,
+};
+
+export default dummyData;
